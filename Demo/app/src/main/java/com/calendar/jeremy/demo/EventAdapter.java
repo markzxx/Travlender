7cf0b3d1819e31ca7353aa69618e3827eb06f3d1
jeremyzhangsq <11510580@mail.sustc.edu.cn>
2017-10-19 20:59:01 +0800

    zhangshiqi

---
+++ Demo/app/src/main/java/com/calendar/jeremy/demo/EventAdapter.java
@@ -0,0 +1,55 @@
+package com.calendar.jeremy.demo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+
+public class EventAdapter extends BaseAdapter {
+    private List<Event> events;
+    private Context context;
+    public EventAdapter(Context context, List<Event> events) {
+        super();
+        this.events=events;
+        this.context=context;
+    }
+
+    @Override
+    public int getCount() {
+        return events.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return events.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view==null){
+            view= LayoutInflater.from(context).inflate(R.layout.list_item,viewGroup,false);
+        }
+
+        TextView tvName= (TextView) view.findViewById(R.id.title);
+        TextView tvStart= (TextView) view.findViewById(R.id.start);
+        TextView tvEnd= (TextView) view.findViewById(R.id.end);
+
+        tvName.setText("Event Title  "+events.get(i).getTitle());
+        tvStart.setText("Starr TIME  "+events.get(i).getStart());
+        tvEnd.setText("End Time  "+events.get(i).getEnd());
+        return view;
+    }
+}
