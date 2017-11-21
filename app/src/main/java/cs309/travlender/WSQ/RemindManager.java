c79d4d044d51c417a473a9821f37cbcfd859392c
Alice Wu <imadream@foxmail.com>
2017-11-21 20:09:03 +0800

    the basic of basic of alarm was finished.

--- app/src/main/java/cs309/travlender/WSQ/RemindManager.java
+++ app/src/main/java/cs309/travlender/WSQ/RemindManager.java
@@ -3,16 +3,22 @@ package cs309.travlender.WSQ;
 import cs309.travlender.ZXX.EventManager;
 
 /**
- * Created by alicewu on 11/21/17.
+ * Created by alicewu on 11/21/17.两边传消息的观察者。
  */
 
 public class RemindManager {
     private EventManager eventManager;
 
-    public RemindManager(EventManager e){
+    public RemindManager(EventManager e){//需要可以被后台闹钟唤醒，重新获取所有事件
         eventManager = e;
-        e.setRemindManager();
-    }
+        e.setRemindManager(this);
 
+    }
+    public void update(int eventID){//notify one event with ID
+
+    }
+    public void updateAll(){
+
+    }
 
 }
