4be4b272bb07a8ae1ed0a8a447e86dbdb52681f6
MARKZXX\Administrator <mark.zxx@qq.com>
2017-11-21 21:00:15 +0800

    ZXX 1027

---
+++ app/src/main/java/cs309/travlender/Add_Event/AddEventActivity.java
@@ -0,0 +1,15 @@
+package cs309.travlender.Add_Event;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cs309.travelender.R;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
