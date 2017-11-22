360c1c399ac3e47f64d9942704c1a2e4e93db1a5
jeremyzhangsq <jeremyzhangsq@gmail.com>
2017-11-22 18:47:16 +0800

    ZSQ11.21

--- app/src/main/java/cs309/travlender/Add_Event/ViewEventActivity.java
+++ app/src/main/java/cs309/travlender/Add_Event/ViewEventActivity.java
@@ -5,13 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import cs309.travelender.R;
 import cs309.travlender.ZSQ.Event;
@@ -21,32 +23,57 @@ import cs309.travlender.ZXX.EventManager;
  * Created by Jeremy Zhang PC on 2017/11/21.
  */
 
-public class ViewEventActivity extends Activity implements View.OnClickListener{
+public class ViewEventActivity extends Activity{
 
-    private TextView etTitle,etStart,etEnd,etLocation,etTransport,etRemindtime;
-    private ImageButton btnBack,btnDele;
-    private FloatingActionButton btnChange;
     private int id;
     private EventManager EM;
     private Intent intent;
     private Event currentEvent;
+
+    @Bind(R.id.detail_alarm_title)
+    TextView etTitle;
+    @Bind(R.id.detail_start_time)
+    TextView etStart;
+    @Bind(R.id.detail_end_time)
+    TextView etEnd;
+    @Bind(R.id.detail_alarm_remind)
+    TextView etRemindtime;
+    @Bind(R.id.detail_alarm_local)
+    TextView etLocation;
+    @Bind(R.id.detail_alarm_transport)
+    TextView etTransport;
+
+    @Bind(R.id.update_fab)
+    FloatingActionButton btnChange;
+    @Bind(R.id.tv_delete)
+    ImageButton btnDele;
+    @Bind(R.id.left_alarm_back)
+    ImageButton btnBack;
+
+    @OnClick(R.id.update_fab)
+    void toEdit() {
+        Intent intent=new Intent(ViewEventActivity.this,AddEventActivity.class);
+        intent.putExtra("request","EDIT");
+        intent.putExtra("id",currentEvent.getEventId());
+        startActivityForResult(intent, 0);
+    }
+
+    @OnClick(R.id.tv_delete)
+    void toDelete() {
+        EM.deleteEvent(id);
+        finish();
+    }
+
+    @OnClick(R.id.left_alarm_back)
+    void toBack() {
+        finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_detail);
-
-        //UI 部分
-        etTitle= (TextView) findViewById(R.id.detail_alarm_title);
-        etStart= (TextView) findViewById(R.id.detail_start_time);
-        etEnd= (TextView) findViewById(R.id.detail_end_time);
-        etRemindtime=(TextView) findViewById(R.id.detail_alarm_remind);
-        etLocation= (TextView) findViewById(R.id.detail_alarm_local);
-        etTransport= (TextView) findViewById(R.id.detail_alarm_transport);
-        btnChange= (FloatingActionButton) findViewById(R.id.update_fab);
-        btnDele= (ImageButton) findViewById(R.id.tv_delete);
-        btnBack= (ImageButton) findViewById(R.id.left_alarm_back);
-
-
+        ButterKnife.bind(this);
         EM=new EventManager(this);
         //获取传递过来的intent
         intent=getIntent();
@@ -54,7 +81,8 @@ public class ViewEventActivity extends Activity implements View.OnClickListener{
         SimpleDateFormat format = new SimpleDateFormat("YYYY-MM-dd HH:mm");
 
         id=intent.getExtras().getInt("id");
-        currentEvent = EM.openEvent(id);
+//        currentEvent = EM.openEvent(id);
+        currentEvent= EM.getAllEvent().get(0);
         etTitle.setText(currentEvent.getTitle());
         etStart.setText(format.format(new Timestamp(currentEvent.getStarttime())));
         etEnd.setText(format.format(new Timestamp(currentEvent.getEndtime())));
@@ -62,31 +90,6 @@ public class ViewEventActivity extends Activity implements View.OnClickListener{
         etLocation.setText(currentEvent.getLocation());
         etTransport.setText(currentEvent.getTransport());
 
-        btnBack.setOnClickListener(this);
-        btnDele.setOnClickListener(this);
-        btnChange.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.tv_delete:
-                EM.deleteEvent(id);
-                finish();
-                break;
-            case R.id.left_alarm_back:
-                finish();
-                break;
-            case R.id.update_fab:
-                Intent intent=new Intent(ViewEventActivity.this,AddEventActivity.class);
-                intent.putExtra("request","EDIT");
-                intent.putExtra("id",currentEvent.getEventId());
-                startActivityForResult(intent, 0);
-        }
-
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        onCreate(null);
-    }
 }
