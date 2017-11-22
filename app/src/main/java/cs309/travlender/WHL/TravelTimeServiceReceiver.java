1991cc9b0c63c574aad40c8d7b66ab1292836b42
HanlinWei <30302047+HanlinWei@users.noreply.github.com>
2017-11-22 19:46:59 +0800

    2017-10-27

--- app/src/main/java/cs309/travlender/WHL/TravelTimeServiceReceiver.java
+++ app/src/main/java/cs309/travlender/WHL/TravelTimeServiceReceiver.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class LocationSearchReceiver extends BroadcastReceiver {
+public class TravelTimeServiceReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
