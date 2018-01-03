1c864164d6c87a00d364f4ec65a623fbfdddc28c
MARKZXX\Administrator <mark.zxx@qq.com>
2018-01-04 01:57:02 +0800

    1.3 prefence

--- app/src/main/java/cs309/travlender/WSQ/PrefActivity.java
+++ app/src/main/java/cs309/travlender/WSQ/PrefActivity.java
@@ -14,23 +14,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cs309.travelender.R;
 
 /**
  * Created by alicewu on 12/31/17.
  */
-public class PrefActivity extends PreferenceActivity{
-
-    @OnClick(R.id.back_pref)
-    void back(){
-        finish();
-    }
-    @OnClick(R.id.pref_save)
-    void save(){
-
-        finish();
-    }
+public class PrefActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,58 +34,4 @@ public class PrefActivity extends PreferenceActivity{
         onCreate(null);
     }
 
-
-    class Settings extends PreferenceFragment{
-        SharedPreferences.OnSharedPreferenceChangeListener mChangeListener;
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            mChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-                @Override
-                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-                    //监听到活动后主要在这里调用方法修改数据库
-//                if ("pop-up_win".equals(key) || "notifications_vibrate".equals(key) || "auto_plan".equals(key)) {
-//                    Toast.makeText(mActivity, key + " : change to " + sharedPreferences.getBoolean(key, true), Toast.LENGTH_SHORT).show();
-//                    if("pop-up_win".equals(key)){
-//                        setDB.isPopWin(sharedPreferences.getBoolean(key, true));
-//                    }
-//                    else if ("notifications_vibrate".equals(key)){
-//                        setDB.isVibrate(sharedPreferences.getBoolean(key, true));
-//                    }
-//                    else {
-//                        setDB.isAutoPlan(sharedPreferences.getBoolean(key, true));
-//                    }
-//                } else if ("remind_before".equals(key)) {
-//                    setDB.remind_before(sharedPreferences.getString(key, "Never"));
-//                    findPreference("remind_before").setSummary("remind event " + sharedPreferences.getString(key, "Never") + " before");
-//                } else if ("notifications_ringtone".equals(key)) {
-//                    setDB.modifyRingtone(sharedPreferences.getString(key, "Default ring"));//default ring应修改为系统铃声引用，此处不应为字符串
-//                    findPreference("notifications_ringtone").setSummary(sharedPreferences.getString(key, "Default ring"));
-                }
-//            }
-            };
-            addPreferencesFromResource(R.xml.preference);
-            setContentView(R.layout.pref_layout);
-        }
-
-        //注册活动
-        @Override
-        public void onResume() {
-            super.onResume();
-            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(mChangeListener);
-        }
-
-        //注销活动
-        @Override
-        public void onPause() {
-            super.onPause();
-
-            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(mChangeListener);
-        }
-
-    }
-
 }
-
