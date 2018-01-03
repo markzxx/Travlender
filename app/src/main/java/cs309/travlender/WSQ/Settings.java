7f1aa396a467d09e8aa393dfd669ba6cfd17ba56
Alice Wu <imadream@foxmail.com>
2018-01-03 10:58:22 +0800

    小的修改

--- app/src/main/java/cs309/travlender/WSQ/Settings.java
+++ app/src/main/java/cs309/travlender/WSQ/Settings.java
@@ -26,17 +26,20 @@ public class Settings extends PreferenceFragment {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
                 //监听到活动后主要在这里调用方法修改数据库
-                if ("pop-up_win".equals(key) || "notifications_vibrate".equals(key)) {
+                if ("pop-up_win".equals(key) || "notifications_vibrate".equals(key) || "auto_plan".equals(key)) {
                     Toast.makeText(mActivity, key + " : change to " + sharedPreferences.getBoolean(key, true), Toast.LENGTH_SHORT).show();
                     if("pop-up_win".equals(key)){
                         setDB.isPopWin(sharedPreferences.getBoolean(key, true));
                     }
-                    else{
+                    else if ("notifications_vibrate".equals(key)){
                         setDB.isVibrate(sharedPreferences.getBoolean(key, true));
+                    }
+                    else {
+                        setDB.isAutoPlan(sharedPreferences.getBoolean(key, true));
                     }
                 } else if ("remind_before".equals(key)) {
                     setDB.remind_before(sharedPreferences.getString(key, "Never"));
-                    findPreference("remind_before").setSummary(sharedPreferences.getString(key, "Never"));
+                    findPreference("remind_before").setSummary("remind event " + sharedPreferences.getString(key, "Never") + " before");
                 } else if ("notifications_ringtone".equals(key)) {
                     setDB.modifyRingtone(sharedPreferences.getString(key, "Default ring"));//default ring应修改为系统铃声引用，此处不应为字符串
                     findPreference("notifications_ringtone").setSummary(sharedPreferences.getString(key, "Default ring"));
